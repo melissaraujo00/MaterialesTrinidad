539e4540bf564ba288cde79345506b7de584ea02
melissaraujo00 <daliarivas11@gmail.com>
2025-03-18 15:29:58 -0600

    Modifican de pagina de inicio

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -2,6 +2,7 @@ import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
+
 interface AuthLayoutProps {
     name?: string;
     title?: string;
@@ -15,9 +16,10 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
-                            </div>
+                        <div className="mb-1 flex items-center justify-center rounded-md w-auto h-auto">
+                            <AppLogoIcon className="w-38 h-38 fill-current text-white dark:text-black" />
+                        </div>
+
                             <span className="sr-only">{title}</span>
                         </Link>
 
